d8a0efc3ab819a658d680a2471624bed6630f4ab
okon-emma <okon.emma0@gmail.com>
2023-05-17 17:04:04 -0500

    new scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :posts
   resources :entries
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
